2bd1b7d64417838244918bd27a691e63ee4b3f55
Nathan Page <33336363+ndpage@users.noreply.github.com>
2021-07-28 22:28:50 -0400

    changed text file to actual data

--- week2/assignment.py
+++ week2/assignment.py
@@ -1,19 +1,20 @@
 
 import re # import regular expression module
 
-numList = []  # create list for all numbers
+numList = []  # create and intialize variables
 total = 0
 count = 0
-fhandle = open('sample.txt')  #open handle for sample text file
+
+fhandle = open('act_data.txt')  # open handle for text file
 
 for line in fhandle:
-  numList = re.findall('[0-9]+',line)
+  numList = re.findall('[0-9]+',line)  # finall all integers in the line with at least 1 number
   if len(numList) < 1:
     continue
-  for num in numList:
-    num = int(num)
-    total = total + num
-    count = count + 1
+  for num in numList: 
+    num = int(num)      # convert to int so we can sum the items in the number list
+    total = total + num # sum the total off th numbers
+    count = count + 1   # increment the count of numbers that have been found
 
-print("Found",count, "numbers")
+print("Found",count, "numbers")  # print the resulting count and sum total
 print("Total is", total)
